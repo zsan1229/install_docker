419cd371ae23ccd1a7ca577e87c071275d65160e
zsan1229 <164998048+zsan1229@users.noreply.github.com>
2024-05-18 21:30:36 +0800

    Update and rename debian安装脚本 to install_docker_ubuntu.sh

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -10,13 +10,13 @@ echo "创建 keyrings 目录..."
 sudo install -m 0755 -d /etc/apt/keyrings
 
 echo "下载 Docker 的 GPG 密钥..."
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
 echo "修改密钥文件权限..."
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "添加 Docker APT 仓库源..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "再次更新包索引..."
 sudo apt-get update
